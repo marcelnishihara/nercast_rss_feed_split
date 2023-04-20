6db799a04ad7733d44a4b7410b134c0d939b9996
Marcel Nishihara <marcelnishihara@gmail.com>
2023-04-20 01:09:25 -0300

    v1.0.0: First GCP Cloud Storage tests

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const CloudStorage = require('./src/cloud_storage.js')
 const main = async _ => {
     let cs = new CloudStorage()
     await cs.run()
-    
-    cs.files.forEach(file => {
-        console.info(file.name)
-    })
 }
 
 
--- src/cloud_storage.js
+++ src/cloud_storage.js
@@ -1,14 +1,34 @@
 const { Storage } = require('@google-cloud/storage')
-const NerdCast = require('../src/nerdcast.js')
 const serviceAccount = require('../credentials/cloud_storage.json')
 
 
 class CloudStorage {
+    #storage
     #files = new Array()
 
-    constructor (projectId = 'nercast-rss-feed-split') {
-        this.storage = new Storage({
-            projectId: projectId,
+    constructor (projectId = 'nerdcast-feeds') {
+        this.projectId = projectId
+    }
+
+
+    /**
+     * @type {Storage.bucket.files[]}
+     * @returns {Storage.bucket.files[]} Objects from a given bucket
+     */
+    get files() {
+        return this.#files
+    }
+
+
+    /**
+     * Google Cloud Storage authentication proccess stores a new 
+     * Storage session into the private property ``this.#storage``.
+     * 
+     * @method #authentication
+     */
+    #authentication() {
+        this.#storage = new Storage({
+            projectId: this.projectId,
             credentials: {
                 client_email: serviceAccount.client_email,
                 private_key: serviceAccount.private_key
@@ -17,18 +37,28 @@ class CloudStorage {
     }
 
 
-    get files() {
-        return this.#files
+    /**
+     * Gets the all objects from a bucket named in the parameter and 
+     * populate the private property ``#files``
+     * 
+     * @method #getFilesFromBucket
+     * @param {String} bucketName - Bucket name
+     */
+    async #getFilesFromBucket(bucketName = 'nerdcast-feeds') {
+        [this.#files] = await this.#storage.bucket(bucketName).getFiles()
     }
 
 
-    async #listFiles(bucketName = 'nercast-rss-feed-split') {
-        [this.#files] = await this.storage.bucket(bucketName).getFiles()
-    }
-
-
+    /**
+     * The only public method executes all private ones to create a 
+     * valid RSS feed content for each subject and upload them into the 
+     * bucket, replacing the old files if exists.
+     * 
+     * @method run
+     */
     async run() {
-        await this.#listFiles()
+        this.#authentication()
+        await this.#getFilesFromBucket()
     }
 }
 
